da1bc7fef479c925164da01d8da4fd75655a9d3b
DevExpressExampleBot <fakeEmail>
2023-10-20 13:59:22 +0400

    autogenerate VB

--- VB/DevExpressMvcApplication1/Models/Categories.vb
+++ VB/DevExpressMvcApplication1/Models/Categories.vb
@@ -1,152 +1,165 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Collections
 Imports System.Linq
 Imports System.Web.UI
 
 Public MustInherit Class ItemsData
-	Implements IHierarchicalEnumerable, IEnumerable
-	Private data As IEnumerable
+    Implements IHierarchicalEnumerable, IEnumerable
 
-	Public Sub New()
-		Me.data = GetData()
-	End Sub
+    Private data As IEnumerable
 
-	Public Function GetEnumerator() As IEnumerator Implements System.Collections.IEnumerable.GetEnumerator
-		Return Me.data.GetEnumerator()
-	End Function
-	Public Function GetHierarchyData(ByVal enumeratedItem As Object) As IHierarchyData Implements IHierarchicalEnumerable.GetHierarchyData
-		Return CType(enumeratedItem, IHierarchyData)
-	End Function
+    Public Sub New()
+        data = GetData()
+    End Sub
 
-	Public MustOverride Function GetData() As IEnumerable
+    Public Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Return data.GetEnumerator()
+    End Function
+
+    Public Function GetHierarchyData(ByVal enumeratedItem As Object) As IHierarchyData Implements IHierarchicalEnumerable.GetHierarchyData
+        Return CType(enumeratedItem, IHierarchyData)
+    End Function
+
+    Public MustOverride Function GetData() As IEnumerable
 End Class
 
 Public Class ItemData
-	Implements IHierarchyData
-	Private privateText As String
-	Public Property Text() As String
-		Get
-			Return privateText
-		End Get
-		Protected Set(ByVal value As String)
-			privateText = value
-		End Set
-	End Property
-	Private privateNavigateUrl As String
-	Public Property NavigateUrl() As String
-		Get
-			Return privateNavigateUrl
-		End Get
-		Protected Set(ByVal value As String)
-			privateNavigateUrl = value
-		End Set
-	End Property
+    Implements IHierarchyData
 
-	Public Sub New(ByVal text As String, ByVal navigateUrl As String)
-		Text = text
-		NavigateUrl = navigateUrl
-	End Sub
+    Private _Text As String, _NavigateUrl As String
 
-	' IHierarchyData
-	Private ReadOnly Property IHierarchyData_HasChildren() As Boolean Implements IHierarchyData.HasChildren
-		Get
-			Return HasChildren()
-		End Get
-	End Property
-	Private ReadOnly Property IHierarchyData_Item() As Object Implements IHierarchyData.Item
-		Get
-			Return Me
-		End Get
-	End Property
-	Private ReadOnly Property Path() As String Implements IHierarchyData.Path
-		Get
-			Return NavigateUrl
-		End Get
-	End Property
-	Private ReadOnly Property Type() As String Implements IHierarchyData.Type
-		Get
-			Return Me.GetType().ToString()
-		End Get
-	End Property
-	Private Function GetChildren() As IHierarchicalEnumerable Implements IHierarchyData.GetChildren
-		Return CreateChildren()
-	End Function
-	Private Function GetParent() As IHierarchyData Implements IHierarchyData.GetParent
-		Return Nothing
-	End Function
+    Public Property Text As String
+        Get
+            Return _Text
+        End Get
 
-	Protected Overridable Function HasChildren() As Boolean
-		Return False
-	End Function
-	Protected Overridable Function CreateChildren() As IHierarchicalEnumerable
-		Return Nothing
-	End Function
+        Protected Set(ByVal value As String)
+            _Text = value
+        End Set
+    End Property
+
+    Public Property NavigateUrl As String
+        Get
+            Return _NavigateUrl
+        End Get
+
+        Protected Set(ByVal value As String)
+            _NavigateUrl = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal text As String, ByVal navigateUrl As String)
+        Me.Text = text
+        Me.NavigateUrl = navigateUrl
+    End Sub
+
+    ' IHierarchyData
+    Private ReadOnly Property IHierarchyData_HasChildren As Boolean Implements IHierarchyData.HasChildren
+        Get
+            Return HasChildren()
+        End Get
+    End Property
+
+    Private ReadOnly Property Item As Object Implements IHierarchyData.Item
+        Get
+            Return Me
+        End Get
+    End Property
+
+    Private ReadOnly Property Path As String Implements IHierarchyData.Path
+        Get
+            Return NavigateUrl
+        End Get
+    End Property
+
+    Private ReadOnly Property Type As String Implements IHierarchyData.Type
+        Get
+            Return [GetType]().ToString()
+        End Get
+    End Property
+
+    Private Function GetChildren() As IHierarchicalEnumerable Implements IHierarchyData.GetChildren
+        Return CreateChildren()
+    End Function
+
+    Private Function GetParent() As IHierarchyData Implements IHierarchyData.GetParent
+        Return Nothing
+    End Function
+
+    Protected Overridable Function HasChildren() As Boolean
+        Return False
+    End Function
+
+    Protected Overridable Function CreateChildren() As IHierarchicalEnumerable
+        Return Nothing
+    End Function
 End Class
 
-
 Public Class CategoriesData
-	Inherits ItemsData
-	Public Overrides Function GetData() As IEnumerable
-		Return _
-			From category In NorthwindDataProvider.DB.Categories _
-			Select New CategoryData(category)
-	End Function
+    Inherits ItemsData
+
+    Public Overrides Function GetData() As IEnumerable
+        Return From category In DB.Categories Select New CategoryData(category)
+    End Function
 End Class
 
 Public Class CategoryData
-	Inherits ItemData
-	Private privateCategory As Category
-	Public Property Category() As Category
-		Get
-			Return privateCategory
-		End Get
-		Protected Set(ByVal value As Category)
-			privateCategory = value
-		End Set
-	End Property
+    Inherits ItemData
 
-	Public Sub New(ByVal category As Category)
-		MyBase.New(category.CategoryName, "?CategoryID=" & category.CategoryID)
-		Category = category
-	End Sub
+    Private _Category As Category
 
-	Protected Overrides Overloads Function HasChildren() As Boolean
-		Return True
-	End Function
-	Protected Overrides Function CreateChildren() As IHierarchicalEnumerable
-		Return New ProductsData(Category.CategoryID)
-	End Function
+    Public Property Category As Category
+        Get
+            Return _Category
+        End Get
+
+        Protected Set(ByVal value As Category)
+            _Category = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal category As Category)
+        MyBase.New(category.CategoryName, "?CategoryID=" & category.CategoryID)
+        Me.Category = category
+    End Sub
+
+    Protected Overrides Function HasChildren() As Boolean
+        Return True
+    End Function
+
+    Protected Overrides Function CreateChildren() As IHierarchicalEnumerable
+        Return New ProductsData(Category.CategoryID)
+    End Function
 End Class
 
 Public Class ProductsData
-	Inherits ItemsData
-	Private privateCategoryID As Integer
-	Public Property CategoryID() As Integer
-		Get
-			Return privateCategoryID
-		End Get
-		Protected Set(ByVal value As Integer)
-			privateCategoryID = value
-		End Set
-	End Property
+    Inherits ItemsData
 
-	Public Sub New(ByVal categoryID As Integer)
-		MyBase.New()
-		CategoryID = categoryID
-	End Sub
+    Private _CategoryID As Integer
 
-	Public Overrides Function GetData() As IEnumerable
-		Return _
-			From product In NorthwindDataProvider.DB.Products _
-			Where product.CategoryID.Equals(CategoryID) _
-			Select New ProductData(product)
-	End Function
+    Public Property CategoryID As Integer
+        Get
+            Return _CategoryID
+        End Get
+
+        Protected Set(ByVal value As Integer)
+            _CategoryID = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal categoryID As Integer)
+        MyBase.New()
+        Me.CategoryID = categoryID
+    End Sub
+
+    Public Overrides Function GetData() As IEnumerable
+        Return From product In DB.Products Where product.CategoryID = CategoryID Select New ProductData(product)
+    End Function
 End Class
 
 Public Class ProductData
-	Inherits ItemData
-	Public Sub New(ByVal product As Product)
-		MyBase.New(product.ProductName, "?CategoryID=" & product.CategoryID & "&ProductID=" & product.ProductID)
-	End Sub
+    Inherits ItemData
+
+    Public Sub New(ByVal product As Product)
+        MyBase.New(product.ProductName, "?CategoryID=" & product.CategoryID & "&ProductID=" & product.ProductID)
+    End Sub
 End Class
--- VB/DevExpressMvcApplication1/Models/Northwind.vb
+++ VB/DevExpressMvcApplication1/Models/Northwind.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.Collections.Generic
@@ -9,253 +8,167 @@ Imports System.Linq
 Imports System.Web
 Imports System.Web.UI
 
+Public Module NorthwindDataProvider
 
-	Public NotInheritable Class NorthwindDataProvider
-		Private Const NorthwindDataContextKey As String = "DXNorthwindDataContext"
+    Const NorthwindDataContextKey As String = "DXNorthwindDataContext"
 
-		Private Sub New()
-		End Sub
-		Public Shared ReadOnly Property DB() As NorthwindDataContext
-			Get
-				If HttpContext.Current.Items(NorthwindDataContextKey) Is Nothing Then
-					HttpContext.Current.Items(NorthwindDataContextKey) = New NorthwindDataContext()
-				End If
-				Return CType(HttpContext.Current.Items(NorthwindDataContextKey), NorthwindDataContext)
-			End Get
-		End Property
+    Public ReadOnly Property DB As NorthwindDataContext
+        Get
+            If System.Web.HttpContext.Current.Items(NorthwindDataProvider.NorthwindDataContextKey) Is Nothing Then System.Web.HttpContext.Current.Items(NorthwindDataProvider.NorthwindDataContextKey) = New NorthwindDataContext()
+            Return CType(System.Web.HttpContext.Current.Items(NorthwindDataProvider.NorthwindDataContextKey), NorthwindDataContext)
+        End Get
+    End Property
 
-		Private Shared Function CalculateAveragePrice(ByVal categoryID As Integer) As Double
-			Return CDbl(( _
-						From product In DB.Products _
-						Where product.CategoryID.Equals(categoryID) _
-						Select product).Average(Function(s) s.UnitPrice))
-		End Function
-		Public Shared Function GetCategories() As IEnumerable
-			Return _
-				From category In DB.Categories _
-				Select category
-		End Function
-		Public Shared Function GetCategoriesNames() As IEnumerable
-			Return _
-				From category In DB.Categories _
-				Select category.CategoryName
-		End Function
-		Public Shared Function GetCategoriesAverage() As IEnumerable
-			Return _
-				From category In DB.Categories _
-				Select New With {Key category.CategoryName, Key .AvgPrice = CalculateAveragePrice(category.CategoryID)}
-		End Function
-		Public Shared Function GetCustomers() As IEnumerable
-			Return _
-				From customer In DB.Customers _
-				Select customer
-		End Function
-		Public Shared Function GetProducts() As IEnumerable
-			Return _
-				From product In DB.Products _
-				Select product
-		End Function
-		Public Shared Function GetProducts(ByVal categoryName As String) As IEnumerable
-			Return _
-				From product In DB.Products _
-				Join category In DB.Categories On product.CategoryID Equals category.CategoryID _
-				Where category.CategoryName = categoryName _
-				Select product
-		End Function
-		Public Shared Function GetEmployees() As IEnumerable
-			Return _
-				From employee In DB.Employees _
-				Select employee
-		End Function
-		Public Shared Function GetEmployeePhoto(ByVal employeeId As Integer) As Binary
-			Return ( _
-					From employee In DB.Employees _
-					Where employee.EmployeeID.Equals(employeeId) _
-					Select employee.Photo).SingleOrDefault()
-		End Function
-		Public Shared Function GetEmployeeNotes(ByVal employeeId As Integer) As String
-			Return ( _
-					From employee In DB.Employees _
-					Where employee.EmployeeID.Equals(employeeId) _
-					Select employee.Notes).Single()
-		End Function
-		Public Shared Function GetOrders() As IEnumerable
-			Return _
-				From order In DB.Orders _
-				Select order
-		End Function
-		Public Shared Function GetInvoices() As IEnumerable
-			Return _
-				From invoice In DB.Invoices _
-				Join customer In DB.Customers On invoice.CustomerID Equals customer.CustomerID _
-				Select New With {Key customer.CompanyName, Key invoice.City, Key invoice.Region, Key invoice.Country, Key invoice.UnitPrice, Key invoice.Quantity}
-		End Function
-		Public Shared Function GetFullInvoices() As IEnumerable
-			Return _
-				From invoice In DB.Invoices _
-				Join customer In DB.Customers On invoice.CustomerID Equals customer.CustomerID _
-				Join order In DB.Orders On invoice.OrderID Equals order.OrderID _
-				Select New With {Key .SalesPerson = order.Employee.FirstName & " " & order.Employee.LastName, Key customer.CompanyName, Key invoice.Country, Key invoice.Region, Key invoice.OrderDate, Key invoice.ProductName, Key invoice.UnitPrice, Key invoice.Quantity}
-		End Function
-		Public Shared Function GetInvoices(ByVal customerID As String) As IEnumerable
-			Return _
-				From invoice In DB.Invoices _
-				Where invoice.CustomerID = customerID _
-				Select invoice
-		End Function
+    Private Function CalculateAveragePrice(ByVal categoryID As Integer) As Double
+        Return CDbl((From product In NorthwindDataProvider.DB.Products Where product.CategoryID = categoryID Select product).Average(Function(s) s.UnitPrice))
+    End Function
 
-		Public Shared Function GetEditableProducts() As IList(Of EditableProduct)
-			Dim products As IList(Of EditableProduct) = CType(HttpContext.Current.Session("Products"), IList(Of EditableProduct))
+    Public Function GetCategories() As IEnumerable
+        Return From category In NorthwindDataProvider.DB.Categories Select category
+    End Function
 
-			If products Is Nothing Then
-				products = ( _
-						From product In DB.Products _
-						Select New EditableProduct With {.ProductID = product.ProductID, .ProductName = product.ProductName, .CategoryID = product.CategoryID, .QuantityPerUnit = product.QuantityPerUnit, .UnitPrice = product.UnitPrice, .UnitsInStock = product.UnitsInStock, .Discontinued = product.Discontinued}).ToList()
-				HttpContext.Current.Session("Products") = products
-			End If
-			Return products
-		End Function
-		Public Shared Function GetEditableProduct(ByVal productID As Integer) As EditableProduct
-			Return ( _
-					From product In GetEditableProducts() _
-					Where product.ProductID = productID _
-					Select product).FirstOrDefault()
-		End Function
-		Public Shared Function GetNewEditableProductID() As Integer
-			Dim lastProduct As EditableProduct = ( _
-					From product In GetEditableProducts() _
-					Select product).Last()
-			Return If((lastProduct IsNot Nothing), lastProduct.ProductID + 1, 0)
-		End Function
-		Public Shared Sub DeleteProduct(ByVal productID As Integer)
-			Dim product As EditableProduct = GetEditableProduct(productID)
-			If product IsNot Nothing Then
-				GetEditableProducts().Remove(product)
-			End If
-		End Sub
-		Public Shared Sub InsertProduct(ByVal product As EditableProduct)
-			Dim editProduct As New EditableProduct()
-			editProduct.ProductID = GetNewEditableProductID()
-			editProduct.ProductName = product.ProductName
-			editProduct.CategoryID = product.CategoryID
-			editProduct.QuantityPerUnit = product.QuantityPerUnit
-			editProduct.UnitPrice = product.UnitPrice
-			editProduct.UnitsInStock = product.UnitsInStock
-			editProduct.Discontinued = product.Discontinued
-			GetEditableProducts().Add(editProduct)
-		End Sub
-		Public Shared Sub UpdateProduct(ByVal product As EditableProduct)
-			Dim editProduct As EditableProduct = GetEditableProduct(product.ProductID)
-			If editProduct IsNot Nothing Then
-				editProduct.ProductName = product.ProductName
-				editProduct.CategoryID = product.CategoryID
-				editProduct.QuantityPerUnit = product.QuantityPerUnit
-				editProduct.UnitPrice = product.UnitPrice
-				editProduct.UnitsInStock = product.UnitsInStock
-				editProduct.Discontinued = product.Discontinued
-			End If
-		End Sub
+    Public Function GetCategoriesNames() As IEnumerable
+        Return From category In NorthwindDataProvider.DB.Categories Select category.CategoryName
+    End Function
 
-		Public Shared Function GetEmployeesList() As IEnumerable
-			Return _
-				From employee In DB.Employees _
-				Select New With {Key .ID = employee.EmployeeID, Key .Name = employee.LastName & " " & employee.FirstName}
-		End Function
-		Public Shared Function GetFirstEmployeeID() As Integer
-			Return ( _
-					From employee In DB.Employees _
-					Select employee.EmployeeID).First()
-		End Function
-		Public Shared Function GetEmployee(ByVal employeeId As Integer) As Employee
-			Return ( _
-					From employee In DB.Employees _
-					Where employeeId.Equals(employee.EmployeeID) _
-					Select employee).Single()
-		End Function
-		Public Shared Function GetOrders(ByVal employeeID As Integer) As IEnumerable
-			Return _
-				From order In DB.Orders _
-				Where order.EmployeeID.Equals(employeeID) _
-				Join order_detail In DB.Order_Details On order.OrderID Equals order_detail.OrderID _
-				Join customer In DB.Customers On order.CustomerID Equals customer.CustomerID _
-				Select New With {Key order.OrderID, Key order.ShipName, Key order_detail.Quantity, Key order_detail.UnitPrice, Key customer.ContactName, Key customer.CompanyName, Key customer.City, Key customer.Address, Key customer.Phone, Key customer.Fax}
-		End Function
-	End Class
+    Public Function GetCategoriesAverage() As IEnumerable
+        Return From category In NorthwindDataProvider.DB.Categories Select New With {category.CategoryName, .AvgPrice = NorthwindDataProvider.CalculateAveragePrice(category.CategoryID)}
+    End Function
 
-	Public Class EditableProduct
-		Private privateProductID As Integer
-		Public Property ProductID() As Integer
-			Get
-				Return privateProductID
-			End Get
-			Set(ByVal value As Integer)
-				privateProductID = value
-			End Set
-		End Property
+    Public Function GetCustomers() As IEnumerable
+        Return From customer In NorthwindDataProvider.DB.Customers Select customer
+    End Function
 
-		Private privateProductName As String
-		<Required(ErrorMessage := "Product Name is required"), StringLength(50, ErrorMessage := "Must be under 50 characters")> _
-		Public Property ProductName() As String
-			Get
-				Return privateProductName
-			End Get
-			Set(ByVal value As String)
-				privateProductName = value
-			End Set
-		End Property
+    Public Function GetProducts() As IEnumerable
+        Return From product In NorthwindDataProvider.DB.Products Select product
+    End Function
 
-		Private privateCategoryID? As Integer
-		<Required(ErrorMessage := "Category is required")> _
-		Public Property CategoryID() As Integer?
-			Get
-				Return privateCategoryID
-			End Get
-			Set(ByVal value? As Integer)
-				privateCategoryID = value
-			End Set
-		End Property
+    Public Function GetProducts(ByVal categoryName As String) As IEnumerable
+        Return From product In NorthwindDataProvider.DB.Products Join category In NorthwindDataProvider.DB.Categories On product.CategoryID Equals category.CategoryID Where Equals(category.CategoryName, categoryName) Select product
+    End Function
 
-		Private privateQuantityPerUnit As String
-		<StringLength(100, ErrorMessage := "Must be under 100 characters")> _
-		Public Property QuantityPerUnit() As String
-			Get
-				Return privateQuantityPerUnit
-			End Get
-			Set(ByVal value As String)
-				privateQuantityPerUnit = value
-			End Set
-		End Property
+    Public Function GetEmployees() As IEnumerable
+        Return From employee In NorthwindDataProvider.DB.Employees Select employee
+    End Function
 
-		Private privateUnitPrice? As Decimal
-		<Range(0, 10000, ErrorMessage := "Must be between 0 and 10000$")> _
-		Public Property UnitPrice() As Decimal?
-			Get
-				Return privateUnitPrice
-			End Get
-			Set(ByVal value? As Decimal)
-				privateUnitPrice = value
-			End Set
-		End Property
+    Public Function GetEmployeePhoto(ByVal employeeId As Integer) As Binary
+        Return(From employee In NorthwindDataProvider.DB.Employees Where employee.EmployeeID = employeeId Select employee.Photo).SingleOrDefault()
+    End Function
 
-		Private privateUnitsInStock? As Short
-		<Range(0, 1000, ErrorMessage := "Must be between 0 and 1000")> _
-		Public Property UnitsInStock() As Short?
-			Get
-				Return privateUnitsInStock
-			End Get
-			Set(ByVal value? As Short)
-				privateUnitsInStock = value
-			End Set
-		End Property
+    Public Function GetEmployeeNotes(ByVal employeeId As Integer) As String
+        Return(From employee In NorthwindDataProvider.DB.Employees Where employee.EmployeeID = employeeId Select employee.Notes).[Single]()
+    End Function
 
-		Private discontinued_Renamed? As Boolean
-		Public Property Discontinued() As Boolean?
-			Get
-				Return discontinued_Renamed
-			End Get
-			Set(ByVal value? As Boolean)
-				discontinued_Renamed = If(value Is Nothing, False, value)
-			End Set
-		End Property
-	End Class
+    Public Function GetOrders() As IEnumerable
+        Return From order In NorthwindDataProvider.DB.Orders Select order
+    End Function
 
+    Public Function GetInvoices() As IEnumerable
+        Return From invoice In NorthwindDataProvider.DB.Invoices Join customer In NorthwindDataProvider.DB.Customers On invoice.CustomerID Equals customer.CustomerID Select New With {customer.CompanyName, invoice.City, invoice.Region, invoice.Country, invoice.UnitPrice, invoice.Quantity}
+    End Function
+
+    Public Function GetFullInvoices() As IEnumerable
+        Return From invoice In NorthwindDataProvider.DB.Invoices Join customer In NorthwindDataProvider.DB.Customers On invoice.CustomerID Equals customer.CustomerID Join order In NorthwindDataProvider.DB.Orders On invoice.OrderID Equals order.OrderID Select New With {.SalesPerson = order.Employee.FirstName & " " & order.Employee.LastName, customer.CompanyName, invoice.Country, invoice.Region, invoice.OrderDate, invoice.ProductName, invoice.UnitPrice, invoice.Quantity}
+    End Function
+
+    Public Function GetInvoices(ByVal customerID As String) As IEnumerable
+        Return From invoice In NorthwindDataProvider.DB.Invoices Where Equals(invoice.CustomerID, customerID) Select invoice
+    End Function
+
+    Public Function GetEditableProducts() As IList(Of EditableProduct)
+        Dim products As System.Collections.Generic.IList(Of EditableProduct) = CType(System.Web.HttpContext.Current.Session("Products"), System.Collections.Generic.IList(Of EditableProduct))
+        If products Is Nothing Then
+            products =(From product In NorthwindDataProvider.DB.Products Select New EditableProduct With {.ProductID = product.ProductID, .ProductName = product.ProductName, .CategoryID = product.CategoryID, .QuantityPerUnit = product.QuantityPerUnit, .UnitPrice = product.UnitPrice, .UnitsInStock = product.UnitsInStock, .Discontinued = product.Discontinued}).ToList()
+            System.Web.HttpContext.Current.Session("Products") = products
+        End If
+
+        Return products
+    End Function
+
+    Public Function GetEditableProduct(ByVal productID As Integer) As EditableProduct
+        Return(From product In NorthwindDataProvider.GetEditableProducts() Where product.ProductID = productID Select product).FirstOrDefault()
+    End Function
+
+    Public Function GetNewEditableProductID() As Integer
+        Dim lastProduct As EditableProduct =(From product In NorthwindDataProvider.GetEditableProducts() Select product).Last()
+        Return If((lastProduct IsNot Nothing), lastProduct.ProductID + 1, 0)
+    End Function
+
+    Public Sub DeleteProduct(ByVal productID As Integer)
+        Dim product As EditableProduct = NorthwindDataProvider.GetEditableProduct(productID)
+        If product IsNot Nothing Then NorthwindDataProvider.GetEditableProducts().Remove(product)
+    End Sub
+
+    Public Sub InsertProduct(ByVal product As EditableProduct)
+        Dim editProduct As EditableProduct = New EditableProduct()
+        editProduct.ProductID = NorthwindDataProvider.GetNewEditableProductID()
+        editProduct.ProductName = product.ProductName
+        editProduct.CategoryID = product.CategoryID
+        editProduct.QuantityPerUnit = product.QuantityPerUnit
+        editProduct.UnitPrice = product.UnitPrice
+        editProduct.UnitsInStock = product.UnitsInStock
+        editProduct.Discontinued = product.Discontinued
+        NorthwindDataProvider.GetEditableProducts().Add(editProduct)
+    End Sub
+
+    Public Sub UpdateProduct(ByVal product As EditableProduct)
+        Dim editProduct As EditableProduct = NorthwindDataProvider.GetEditableProduct(product.ProductID)
+        If editProduct IsNot Nothing Then
+            editProduct.ProductName = product.ProductName
+            editProduct.CategoryID = product.CategoryID
+            editProduct.QuantityPerUnit = product.QuantityPerUnit
+            editProduct.UnitPrice = product.UnitPrice
+            editProduct.UnitsInStock = product.UnitsInStock
+            editProduct.Discontinued = product.Discontinued
+        End If
+    End Sub
+
+    Public Function GetEmployeesList() As IEnumerable
+        Return From employee In NorthwindDataProvider.DB.Employees Select New With {.ID = employee.EmployeeID, .Name = employee.LastName & " " & employee.FirstName}
+    End Function
+
+    Public Function GetFirstEmployeeID() As Integer
+        Return(From employee In NorthwindDataProvider.DB.Employees Select employee.EmployeeID).First(Of Integer)()
+    End Function
+
+    Public Function GetEmployee(ByVal employeeId As Integer) As Employee
+        Return(From employee In NorthwindDataProvider.DB.Employees Where employeeId = employee.EmployeeID Select employee).[Single](Of Employee)()
+    End Function
+
+    Public Function GetOrders(ByVal employeeID As Integer) As IEnumerable
+        Return From order In NorthwindDataProvider.DB.Orders Where order.EmployeeID = employeeID Join order_detail In NorthwindDataProvider.DB.Order_Details On order.OrderID Equals order_detail.OrderID Join customer In NorthwindDataProvider.DB.Customers On order.CustomerID Equals customer.CustomerID Select New With {order.OrderID, order.ShipName, order_detail.Quantity, order_detail.UnitPrice, customer.ContactName, customer.CompanyName, customer.City, customer.Address, customer.Phone, customer.Fax}
+    End Function
+End Module
+
+Public Class EditableProduct
+
+    Public Property ProductID As Integer
+
+    <System.ComponentModel.DataAnnotations.RequiredAttribute(ErrorMessage:="Product Name is required")>
+    <System.ComponentModel.DataAnnotations.StringLengthAttribute(50, ErrorMessage:="Must be under 50 characters")>
+    Public Property ProductName As String
+
+    <System.ComponentModel.DataAnnotations.RequiredAttribute(ErrorMessage:="Category is required")>
+    Public Property CategoryID As Integer?
+
+    <System.ComponentModel.DataAnnotations.StringLengthAttribute(100, ErrorMessage:="Must be under 100 characters")>
+    Public Property QuantityPerUnit As String
+
+    <System.ComponentModel.DataAnnotations.RangeAttribute(0, 10000, ErrorMessage:="Must be between 0 and 10000$")>
+    Public Property UnitPrice As Decimal?
+
+    <System.ComponentModel.DataAnnotations.RangeAttribute(0, 1000, ErrorMessage:="Must be between 0 and 1000")>
+    Public Property UnitsInStock As Short?
+
+    Private discontinuedField As Boolean?
+
+    Public Property Discontinued As Boolean?
+        Get
+            Return Me.discontinuedField
+        End Get
+
+        Set(ByVal value As Boolean?)
+            Me.discontinuedField = If(value Is Nothing, False, value)
+        End Set
+    End Property
+End Class
